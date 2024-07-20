7caa6986994a56f16149f04cff105088526bd580
SAIDINESH A <saidinesh2662@gmail.com>
2024-07-20 20:11:41 +0530

    SQL6

---
+++ number of unique subjects taught by each teacher.sql
@@ -0,0 +1,20 @@
+/*Teacher table:
++------------+------------+---------+
+| teacher_id | subject_id | dept_id |
++------------+------------+---------+
+| 1          | 2          | 3       |
+| 1          | 2          | 4       |
+| 1          | 3          | 3       |
+| 2          | 1          | 1       |
+| 2          | 2          | 1       |
+| 2          | 3          | 1       |
+| 2          | 4          | 1       |
++------------+------------+---------+
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+
+Return the result table in any order.
+*/
+
+select teacher_id,
+count(distinct subject_id) as cnt from teacher
+group by teacher_id
